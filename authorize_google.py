2247bd4f801e2bb2acc708cbc7532b4dd84efbb8
alex <prajwalchhetri010@gmail.com>
2025-07-16 14:56:59 +0545

    feat:updates from discord

--- authorize_google.py
+++ authorize_google.py
@@ -19,10 +19,14 @@ def main():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'client_secret.json', SCOPES)
-            creds = flow.run_console()
+            auth_url, _ = flow.authorization_url(prompt='consent')
+            print("Please go to this URL and authorize the application:\n", auth_url)
+            code = input("Enter the authorization code: ")
+            flow.fetch_token(code=code)
+            creds = flow.credentials
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     print("Authentication successful! token.pickle created.")
 
 if __name__ == '__main__':
-    main() 
+    main()
